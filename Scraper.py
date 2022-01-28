3afe3ec41569ff9aa6d3bebdf3a15a575bb091de
Victor Nguyen <46537385+NVectors@users.noreply.github.com>
2022-01-28 15:24:58 -0700

    Place Reddit comments into a dataframe

--- Scraper.py
+++ Scraper.py
@@ -1,7 +1,6 @@
 from decouple import config
 import praw
-#import pandas as pd
-
+import pandas as pd
 
 # Read-only Reddit instance
 reddit = praw.Reddit(
@@ -9,6 +8,7 @@ reddit = praw.Reddit(
     client_secret= config('CLIENT_SECRET'),
     user_agent= config('USER_AGENT'))
 
+# Test
 #print(reddit.read_only)
 # Output: True
 
@@ -16,13 +16,18 @@ reddit = praw.Reddit(
 url = "https://www.reddit.com/r/AskReddit/comments/s9oaa6/what_is_the_most_beautiful_song_you_have_ever/"
 submission = reddit.submission(url=url)
 
+# Reduce comments returned and sort comments by "top"
+submission.comment_sort = 'top'
+submission.comment_limit = 10000
+
 # Remove all "MoreComments" instances
 submission.comments.replace_more(limit=0)
 
-# Print out comments
-for top_level_comment in submission.comments:
-   print(top_level_comment.body)
-     
+# Comment id and comment body into a dataframe
+comments = submission.comments
+df_rows = [[ comment.id, comment.body] for comment in comments]
+df = pd.DataFrame(df_rows, columns=['Comment ID','Body'])
+df.to_csv('./data/comments_raw.csv')
 
 
     
