61a3b5b7355e26fb7695a69f85db3ce6f26b973f
Victor Nguyen <46537385+NVectors@users.noreply.github.com>
2022-02-11 13:02:34 -0700

    Made functions

--- Scraper.py
+++ Scraper.py
@@ -8,28 +8,27 @@ reddit = praw.Reddit(
     client_secret= config('CLIENT_SECRET'),
     user_agent= config('USER_AGENT'))
 
-# Test
-#print(reddit.read_only)
-# Output: True
+def get_comment(url):
+    submission = reddit.submission(url=url)
+    submission.comment_sort = 'top'
+    submission.comment_limit = 10000
+    submission.comments.replace_more(limit=0)
+    
+    for comment in submission.comments:
+        read_comment(comment)
 
-# Obtain submission object
-url = "https://www.reddit.com/r/AskReddit/comments/s9oaa6/what_is_the_most_beautiful_song_you_have_ever/"
-submission = reddit.submission(url=url)
+def read_comment(comment):
+    if hasattr(comment, 'body'):
+        print(comment.body)
 
-# Reduce comments returned and sort comments by "top"
-submission.comment_sort = 'top'
-submission.comment_limit = 10000
-
-# Remove all "MoreComments" instances
-submission.comments.replace_more(limit=0)
-
-# Comment id and comment body into a dataframe
-comments = submission.comments
-df_rows = [[ comment.id, comment.body] for comment in comments]
-df = pd.DataFrame(df_rows, columns=['Comment ID','Body'])
-df.to_csv('./data/comments_raw.csv')
+def main():
+    # Obtain submission object
+    url = "https://www.reddit.com/r/AskReddit/comments/s9oaa6/what_is_the_most_beautiful_song_you_have_ever/"
+    get_comment(url)
 
 
+if __name__ == '__main__':
+    main()
     
 
 
